546e6e29234f3d7f782cbb2baae8a3aecf3e1056
iamayushy <ayushhuns@gmail.com>
2022-09-13 19:26:05 +0530

    neomorp

---
+++ src/components/Banner.jsx
@@ -0,0 +1,13 @@
+import zoomen from '../assets/zeet.svg'
+
+const Banner = () => {
+    return(
+        <div>
+            <section className='logo-box'>
+                <img src={zoomen} alt="" />
+            </section>
+        </div>
+    )
+}
+
+export {Banner}
---
+++ src/page/ViewPage.jsx
@@ -0,0 +1,11 @@
+import { Banner } from "../components/Banner"
+
+const ViewPage = () => {
+    return(
+        <div>
+            <Banner/>
+        </div>
+    )
+}
+
+export {ViewPage}
